2b994621c5bfbc0ef4ac951cc2122f4d3ab77eaf
pedroexpedito <expeditocaf00@gmail.com>
2021-11-17 20:51:06 -0300

    Wed 17 Nov 2021 08:51:06 PM -03

--- PIM-VIII/dotnet/wwwroot/js/site.js
+++ PIM-VIII/dotnet/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//
+console.log("OI");
+
+var telefones = document.getElementById("telefones");
+
+console.log(telefones);
