b1bc3e70731f8005ae99dc73fdc2bfae3952e843
pedroexpedito <expeditocaf00@gmail.com>
2021-11-21 09:21:02 -0300

    Sun 21 Nov 2021 09:21:01 AM -03

--- PIM-VIII/dotnet/Program.cs
+++ PIM-VIII/dotnet/Program.cs
@@ -1,7 +1,5 @@
-using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using trabalho.Models;
 
 namespace trabalho
 {
@@ -9,35 +7,6 @@ namespace trabalho
   {
     public static void Main(string[] args)
     {
-
-     // var enderecoDAO = new EnderecoDAO();
-     //
-     // var enderecos = enderecoDAO.getAll();
-     //
-     // var endereco1 = new Endereco("HASDAS", 1, "863782", "centro", "cafesal", "parana");
-     // enderecoDAO.remove(1);
-     // enderecoDAO.create(endereco1);
-     //
-     // Console.WriteLine(enderecoDAO.get(8).ToString());
-     // Console.WriteLine("CHEGOU");
-     // foreach( var e in enderecos) {
-     //   Console.WriteLine(e);
-     // }
-     //
-     PessoaDAO pessoaDAO = new PessoaDAO();
-
-     // var telefoneTipo = new TelefoneTipo(2, "comercial");
-     // var telefone = new Telefone(3,8374323, 43, telefoneTipo);
-     //
-     // List<Telefone> telefones = new List<Telefone>();
-     // telefones.Add(telefone);
-     //
-     // var pessoa = new Pessoa("Marcos","293838293", endereco1, telefones); 
-     var pessoas = pessoaDAO.getAll();
-
-     foreach( Pessoa p in pessoas) {
-      Console.WriteLine(p.ToString());
-     }
 
      CreateHostBuilder(args).Build().Run();
 
